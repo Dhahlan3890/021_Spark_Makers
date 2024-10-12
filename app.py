71054b77e63b231d3dffa2a922084a31c6203b0c
Dhahlan3890 <dhahlan2000@gmail.com>
2024-10-12 12:56:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Election Helper", page_icon="🏠")
 
-st.title("Welcome to the Main Page")
+st.title("VoteCast Pro")
 
 st.write("""
 This is the main page of our Election helper app. Use the sidebar to navigate to other pages:
